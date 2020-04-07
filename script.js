e476a32ec4abfeefe73929314583f1802faecd5c
Ashrut Sharma <ashrut05@protonmail.com>
2020-04-07 18:42:04 +0530

    Fixed API call

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-if (event.keyCode === 13) {
-   document.getElementById("search-button").click();
-  };
-
 function getCity() {
     document.getElementById("icon").innerHTML = "";
     document.getElementById("weather").innerHTML = "";
@@ -10,7 +6,7 @@ function getCity() {
     var city= document.getElementById("city").value;
     console.log(city);
 
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=f25c4a8b058a9f156f78b38c2c410bd9", function(data){
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=f25c4a8b058a9f156f78b38c2c410bd9", function(data){
     console.log(data);
 
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
