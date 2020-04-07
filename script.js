ada6d9a1220c9c67dd5575b277ed680a2ae1f627
Ashrut Sharma <ashrut05@protonmail.com>
2020-04-07 19:37:40 +0530

    CSS Styling for output

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ function getCity() {
     document.getElementById("temp").innerHTML = "";
 
     var city= document.getElementById("city").value;
-    console.log(city);
 
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=f25c4a8b058a9f156f78b38c2c410bd9", function(data){
-    console.log(data);
 
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var weather=data.weather[0].main;
@@ -15,7 +13,7 @@ function getCity() {
 
     $("#icon").attr("src", icon);
     document.getElementById("weather").innerHTML = weather;
-    document.getElementById("temp").innerHTML = temp;
+    document.getElementById("temp").innerHTML = temp + "°C";
 
     document.getElementById("city").value = "";
     document.getElementById("weather-container").style.display="block";
